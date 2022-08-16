d93577da5137e97c7d1f645bc2a10dd3f0018d38
GetAwayCoxn <66495755+GetAwayCoxn@users.noreply.github.com>
2022-08-16 19:41:48 -0400

    --THtracker 1.02 - added functions to calculate your base TH from current gear instead of assuming max

--- addons/thtracker/README.md
+++ addons/thtracker/README.md
@@ -12,9 +12,7 @@ Ashita v4 addon for FFXI to track TH levels on mobs.
 
 ## Notes:
 
--Currently assumes you are hitting the max TH+ from gear so base TH8 as THF99 and TH4 as anything else
-
--This reads from the chat log so you still have to have your white damage visible for this to work, however will work with and without the addon SimpleLog loaded.
+-This reads from the chat log so you still have to have your white damage visible for this to work
 
 -Will currently recognize any melee hits or ranged attacks to establish first TH/claim, and of course TH procs after that on THF main, so if you are running around on BLU/THF subductioning stuff in TH gear this will not currently recognize your TH4 on everything
 
@@ -22,9 +20,7 @@ Ashita v4 addon for FFXI to track TH levels on mobs.
 
 -Standing by for bugs
 
--Should add more ways for the addon to recognize initial action (mostly magic), kinda not needed though imo
-
--Should add function to establish your actual TH+ from gear instead of assuming max
+-Should add more ways for the addon to recognize initial action (mostly magic), kinda not needed at retail though imo
 
 ## Current Text Overlay: 
 
--- addons/thtracker/thtracker.lua
+++ addons/thtracker/thtracker.lua
@@ -1,6 +1,6 @@
 addon.name      = 'thtracker';
 addon.author    = 'GetAwayCoxn';
-addon.version   = '1.01';
+addon.version   = '1.02';
 addon.desc      = 'Tracks TH on mobs, this is not a port';
 addon.link      = 'https://github.com/GetAwayCoxn/';
 
@@ -27,6 +27,71 @@ local defaults = T{
 		color = 0xFF000000,
 	}
 };
+local Towns = T{'Tavnazian Safehold','Al Zahbi','Aht Urhgan Whitegate','Nashmau','Southern San d\'Oria [S]','Bastok Markets [S]','Windurst Waters [S]','San d\'Oria-Jeuno Airship','Bastok-Jeuno Airship','Windurst-Jeuno Airship','Kazham-Jeuno Airship','Southern San d\'Oria','Northern San d\'Oria','Port San d\'Oria','Chateau d\'Oraguille','Bastok Mines','Bastok Markets','Port Bastok','Metalworks','Windurst Waters','Windurst Walls','Port Windurst','Windurst Woods','Heavens Tower','Ru\'Lude Gardens','Upper Jeuno','Lower Jeuno','Port Jeuno','Rabao','Selbina','Mhaura','Kazham','Norg','Mog Garden','Celennia Memorial Library','Western Adoulin','Eastern Adoulin',
+};
+local  area = '';
+local gear = T{--keys are slot id's from enums, then itemId:THvalue
+    [0] = T{--copy to [1] for sub slot
+        [16480] = 1,
+        [20618] = 1,
+        [21573] = 1,
+        [21574] = 2,
+        [21575] = 3,
+    },
+    [1] = T{
+        [16480] = 1,
+        [20618] = 1,
+        [21573] = 1,
+        [21574] = 2,
+        [21575] = 3,
+    },
+    [2] = T{},
+    [3] = T{
+        [22299] = 1,
+    },
+    [4] = T{
+        [23713] = 1,
+        [25679] = 1,
+    },
+    [5] = T{
+        [23717] = 2,
+    },
+    [6] = T{
+        [15107] = 1,
+        [14914] = 1,
+        [10695] = 2,
+        [26986] = 2,
+        [26987] = 3,
+        [23202] = 3,
+        [23537] = 4,
+    },
+    [7] = T{
+        [23725] = 1,
+    },
+    [8] = T{
+        [23729] = 1,
+        [11149] = 1,
+        [27421] = 2,
+        [27422] = 3,
+        [23358] = 4,
+    },
+    [9] = T{},
+    [10] = T{
+        [28450] = 1,
+    },
+    [11] = T{},
+    [12] = T{},
+    [13] = T{--copy to [14] for second ring slot
+        [27585] = 1,
+        [26197] = 1,
+    },
+    [14] = T{
+        [27585] = 1,
+        [26197] = 1,
+    },
+    [15] = T{},
+};
+local oseemgear = T{'Chironic','Herculean','Merlinic','Odyssean','Valorous'};
 
 settings.register('settings', 'settings_update', function (s)
     if (s ~= nil) then
@@ -52,7 +117,11 @@ end);
 ashita.events.register('text_in', 'text_in_cb', function(e)
     local me = AshitaCore:GetMemoryManager():GetParty():GetMemberName(0);
     local player = AshitaCore:GetMemoryManager():GetPlayer();
-
+    area = AshitaCore:GetResourceManager():GetString("zones.names", AshitaCore:GetMemoryManager():GetParty():GetMemberZone(0));
+    if (player:GetIsZoning() ~= 0) or (area == nil) or (Towns:contains(area)) then
+        mobs = T{};
+		return;
+	end
     if e.message:contains('Treasure Hunter') or e.message:contains('AE: TH') then
         local index = AshitaCore:GetMemoryManager():GetTarget():GetTargetIndex(0);
         if index == nil then return end;
@@ -67,15 +136,25 @@ ashita.events.register('text_in', 'text_in_cb', function(e)
         if AshitaCore:GetMemoryManager():GetEntity():GetType(index) ~= 2 then return end;
         if mobs[index] ~= nil and mobs[index][3] >=9 then return end;--kick out if already at TH9 or above
         local count = 0;
-        if player:GetMainJob(0) == 6 then--need to add a offset for gear bonus then adjust the for checks below,for now assumes max TH gear if THF main @99, and no TH gear otherwise
+
+        if player:GetMainJob(0) == 6 then
             if player:GetMainJobLevel(0) >= 90 then
-                count = 8;
-            else
-                count = 4;
+                count = 3;
+            elseif player:GetMainJobLevel(0) >= 45 then
+                count = 2;
+            elseif player:GetMainJobLevel(0) >= 15 then
+                count = 1;
             end
-        else
-            count = 4;
+        elseif player:GetSubJob(0) == 6 then
+            if player:GetSubJobLevel(0) >= 45 then
+                count = 2;
+            elseif player:GetSubJobLevel(0) >= 15 then
+                count = 1;
+            end
         end
+        
+        count = count + countGear(player);
+
         if target == nil then return end;
         if mobs[index] ~= nil then 
             if count > mobs[index][3] then
@@ -89,20 +168,20 @@ end);
 
 ashita.events.register('d3d_present', 'present_cb', function ()
     local player = AshitaCore:GetMemoryManager():GetPlayer();
-	if (player:GetIsZoning() ~= 0) then
+    local t = 0;
+    display.text = '';
+    area = AshitaCore:GetResourceManager():GetString("zones.names", AshitaCore:GetMemoryManager():GetParty():GetMemberZone(0));
+
+	if (player:GetIsZoning() ~= 0) or (area == nil) or (Towns:contains(area)) then
         mobs = T{};
 		return;
 	end
-
-    local t = 0;
-    display.text = '';
 
     if not osd.visibleJob[player:GetMainJob(0)] then
         return;
     end
 
     for k,v in pairs(mobs) do
-        
         t = t + 1;
         
         if t == 1 then
@@ -110,13 +189,11 @@ ashita.events.register('d3d_present', 'present_cb', function ()
         end
         
         local mob = GetEntity(k);
-
         if v[2] == 0 then
             display.text = display.text .. osd.deadColor .. '\n' .. v[1] .. '(' .. tostring(k) .. ')  HPP: ' .. tostring(v[2]) .. '  TH: ' .. tostring(v[3]);
         else
             display.text = display.text .. osd.fightColor .. '\n' .. v[1] .. '(' .. tostring(k) .. ')  HPP: ' .. tostring(v[2]) .. '  TH: ' .. tostring(v[3]);
         end
-        
     end
 
 	if display.position_x ~= osd.position_x or display.position_y ~= osd.position_y then--force settings save when dragging the text box
@@ -142,6 +219,8 @@ ashita.events.register('command', 'command_cb', function (e)
         osd.visibleJob[playerJob] = not osd.visibleJob[playerJob];
     elseif args[2] == 'time' then
         osd.displayTime = tonumber(args[3]) or defaults.displayTime;
+    elseif args[2] == 'test' then
+        test();
     end
 end);
 
@@ -164,4 +243,60 @@ function update()
 
         if os.time() - v[5] > osd.displayTime and v[2] == 0 then mobs[k] = nil end;
     end
+end
+
+function countGear(player)
+    local inv = AshitaCore:GetMemoryManager():GetInventory();
+    local total = 0;
+
+    for slot = 0, 15 do
+        local equippedItem = inv:GetEquippedItem(slot);
+        local index = bit.band(equippedItem.Index, 0x00FF);
+        local eqEntry = {};
+        if (index ~= 0) then
+            eqEntry.Container = bit.band(equippedItem.Index, 0xFF00) / 256;
+            eqEntry.Item = inv:GetContainerItem(eqEntry.Container, index);
+            if (eqEntry.Item.Id ~= 0) and (eqEntry.Item.Count ~= 0) then
+                if gear[slot]:haskey(eqEntry.Item.Id) then
+                    total = total + gear[slot][eqEntry.Item.Id]
+                elseif (slot >= 4 and slot <= 8) then
+                    local itm = AshitaCore:GetResourceManager():GetItemById(eqEntry.Item.Id);
+                    if oseemgear:contains(itm.Name[1]:match("%w+")) then
+                        total = total + checkAugment(eqEntry.Item);
+                    end
+                end
+            end
+        end
+    end
+
+    if player:GetMainJob(0) == 6 then
+        if total > 5 then
+            total = 5;
+        end
+    elseif player:GetSubJob(0) == 6 then
+        if total > 4 then
+            total = 4;
+        end
+    end
+
+    return total;
+end
+
+function checkAugment(item)
+    local augType = struct.unpack('B', item.Extra, 1);
+
+    if (augType ~= 2) and (augType ~= 3) then
+        return 0;--kick out if gear not aug'd at all
+    end
+
+    local itemTable = item.Extra:totable();
+    for i = 1,5,1 do
+        local augId = ashita.bits.unpack_be(itemTable, (16 * i), 11);
+        local augValue = ashita.bits.unpack_be(itemTable, (16 * i) + 11, 5);
+        if augId == 147 then --ID for TH augs
+            return augValue + 1; --offest for TH augs is 1
+        end
+    end
+
+    return 0;
 end
